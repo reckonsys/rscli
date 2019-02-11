b1dfdb411ca2ad3c284179966b2029bb20d59feb
dhilipsiva <dhilipsiva@pm.me>
2019-02-11 17:29:38 +0530

    Initial Version

---
+++ rscli.py
@@ -0,0 +1,69 @@
+from shutil import which
+
+import click
+import requests
+from pick import pick
+
+DJANGO = 'django'
+ANGULAR = 'angular'
+FRAMEWORKS = [DJANGO, ANGULAR]
+
+
+@click.group()
+@click.pass_context
+def cli(ctx):
+    '''
+    Reckonsys CLI Toolchain
+    '''
+    ctx.ensure_object(dict)
+
+
+def ensure_gitignore(language):
+    url = (
+        'https://raw.githubusercontent.com/github/'
+        'gitignore/master/%s.gitignore' % language)
+    resp = requests.get(url)
+    with open('.gitignore', 'w') as f:
+        f.write(resp.content.decode())
+
+
+@cli.command()
+def init_django():
+    ensure_gitignore('Python')
+
+
+@cli.command()
+def init_angular():
+    ensure_gitignore('Node')
+
+
+init_map = {}
+for framwork in FRAMEWORKS:
+    init_map[framwork] = globals()["init_%s" % framwork]
+
+
+@cli.command()
+@click.pass_context
+def init(ctx):
+    '''
+    Init
+    '''
+    title = 'Please choose the framwork to init'
+    framwork, _ = pick(FRAMEWORKS, title)
+    ctx.invoke(init_map[framwork])
+
+
+def check_command(cmd):
+    cmd_path = which(cmd)
+    if cmd_path is None:
+        print("[ERROR] Missing Command: %s" % cmd)
+    else:
+        print("[INFO] Using command: %s" % cmd_path)
+    return cmd_path
+
+
+@cli.command()
+@click.pass_context
+def doctor(ctx):
+    [check_command(command) for command in [
+        'pipenv', 'django-admin', 'node', 'yarn']]
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+__VERSION__ = '0.1.0'
+
+setup(
+    name='rscli',
+    version=__VERSION__,
+    description="Reckonsys CLI toolchain",  # NOQA
+    long_description="CLI Toolchain",
+    url='https://github.com/reckonsys/rscli',
+    author='dhilipsiva',
+    author_email='dhilipsiva@pm.me',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='rscli reckonsys cli toolchain',
+    packages=find_packages(),
+    install_requires=[
+        'Click', 'pick', 'requests'
+    ],
+    entry_points='''
+        [console_scripts]
+        rscli=rscli:cli
+    ''',
+)
